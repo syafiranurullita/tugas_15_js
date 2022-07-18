cbfc9156c942d468748c31dfd4a2ddd75fb5deeb
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:40:11 +0700

    tugas 15 js
    untuk Niomic

---
+++ Tugas15/Tugas15.js
@@ -0,0 +1,16 @@
+var x = 8;
+console.log(Math.pow(x,2));//perpangkatan
+
+var y = 6;
+console.log(Math.pow(y,2));
+
+console.log(64 + 36);
+
+
+
+function akar() {
+  console.log(Math.sqrt(64));
+  console.log(Math.sqrt(36)); //untuk mencari akar
+}
+
+akar();
